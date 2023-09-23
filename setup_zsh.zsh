f90797149582a717baba9f28d27f4828243f2dc9
Edwin Scharfe <19590601+HippieHosenHustler@users.noreply.github.com>
2023-09-23 21:22:02 +0200

    check if already linked

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -2,5 +2,10 @@
 
 echo "\n<<<< Starting ZSH Setup >>>>\n"
 
-echo "Enter sudo password to symlink sh to zsh"
-sudo ln -sfv /bin/zsh /private/var/select/sh
+if sh --version | grep -q zsh; then
+    echo 'sh already symlinked to zsh'
+else
+    echo "Enter sudo password to symlink sh to zsh"
+    sudo ln -sfv /bin/zsh /private/var/select/sh
+fi
+
