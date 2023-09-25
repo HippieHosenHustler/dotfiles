e35693f70de51403e82c1ea9cc7fd648a8a87b99
Edwin Scharfe <19590601+HippieHosenHustler@users.noreply.github.com>
2023-09-25 11:30:17 +0200

    add rust setup

---
+++ setup_rust.zsh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+echo "\n<<<< Starting Rust Setup >>>>\n"
+
+# Node versions are managed with 'n', which is in the Brewfile
+# See zshrc for N_PREFIX variable and addition to $path array.
+
+if exists cargo; then
+    echo "Rust and cargo $(cargo --version) already installed\n"
+else
+    echo "Installing rust\n"
+    rustup-init
+fi
+
+cargo install bacon
+
+echo "\nGlobal Cargo Packages installed: \n"
+cargo install --list
