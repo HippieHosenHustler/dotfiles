68d5759469e747039390de744b377732a7e1e808
Edwin Scharfe <19590601+HippieHosenHustler@users.noreply.github.com>
2023-10-17 09:40:12 +0200

    clean up search

--- bin/.local/scripts/tmux-sessionizer.zsh
+++ bin/.local/scripts/tmux-sessionizer.zsh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(tmuxinator ls -n | fzf)
+    selected=$(tmuxinator ls -n | grep -v "tmuxinator projects:" | fzf)
 fi
 
 if [[ -z $selected ]]; then
