46222db6d72b7b33d3fbcd09ba9770a920c68f7d
Edwin Scharfe <19590601+HippieHosenHustler@users.noreply.github.com>
2024-05-22 19:22:34 +0200

    treesitter basic setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.keymap.set('i', "jk", "<esc>")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -22,7 +23,8 @@ local plugins = {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 }
 local opts = {}
 
@@ -30,6 +32,13 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local tsConfig = require("nvim-treesitter.configs")
+tsConfig.setup({
+  ensure_installed = {"lua", "javascript", "apex", "bash", "rust", "soql", "sosl"},
+  highlight = { enable = true },
+  indent = { enable = true },
+})
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
