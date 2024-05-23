aa456a511b2a9e2752e2a3934271b27e50b9a5ca
Edwin Scharfe <19590601+HippieHosenHustler@users.noreply.github.com>
2024-05-23 13:07:12 +0200

    installed sf plugin

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,11 +1,30 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  jdependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = "dracula"
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	jdependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "dracula",
+			},
+			{
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = {
+						"filename",
+						{
+							"require'sf'.get_target_org()",
+							-- cond = function()
+							--   return vim.bo.filetype == 'apex'
+							-- end
+						},
+					},
+					lualine_x = { "encoding", "filetype" },
+					-- lualine_x = {'encoding', 'fileformat', 'filetype'},
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+			},
+		})
+	end,
 }
