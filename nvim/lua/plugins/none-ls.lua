6075b68dbd4d8f5272f3768bb6cbd9dd09d2c7d3
Edwin Scharfe <19590601+HippieHosenHustler@users.noreply.github.com>
2024-05-23 13:15:06 +0200

    none-ls settings for salesforce

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,15 +1,26 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint_d,
-			},
-		})
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format)
-	end,
+        null_ls.builtins.formatting.prettier.with({
+          filetypes = { "apex" },
+          extra_args = { "--plugin=prettier-plugin-apex", "--write" },
+        }),
+
+        null_ls.builtins.diagnostics.eslint_d,
+
+        null_ls.builtins.diagnostics.pmd.with({
+          filetypes = { "apex" },
+          args = { "check", "--dir", "$ROOT", "--format", "json" },
+          extra_args = { "--rulesets", "apex_ruleset.xml" },
+        }),
+      },
+    })
+
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format)
+  end,
 }
