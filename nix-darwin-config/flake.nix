181383435b44561356acc35185e525d77ca7b37b
Edwin Scharfe <19590601+HippieHosenHustler@users.noreply.github.com>
2024-10-10 13:40:58 +0200

    install neovim and jetbrainsmono font

--- nix-darwin-config/flake.nix
+++ nix-darwin-config/flake.nix
@@ -13,8 +13,12 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.vim
+        [ pkgs.neovim
         ];
+
+      fonts.packages = [
+        (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+      ];
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
