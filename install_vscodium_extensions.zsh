424aaca42e4c6d41a87ffaccb3aa0fc733bd0405
Edwin Scharfe <19590601+HippieHosenHustler@users.noreply.github.com>
2025-06-03 16:48:30 +0200

    No longer using vscodium

--- install_vscodium_extensions.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-IFS=$'\n'       # make newlines the only separator
-set -f          # disable globbing
-for i in $(cat < ./codium_extensions); do
-  codium --install-extension $i
-done
